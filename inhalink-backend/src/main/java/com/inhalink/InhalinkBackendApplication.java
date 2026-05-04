8eb9dea5f03587d1004c07decf04b53f2b762aed
youngjun <aakimyoungjun1109@gmail.com>
2026-05-04 20:18:47 +0900

    init: add Spring Boot project

---
+++ inhalink-backend/src/main/java/com/inhalink/InhalinkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.inhalink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhalinkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InhalinkBackendApplication.class, args);
+	}
+
+}
---
+++ inhalink-backend/src/test/java/com/inhalink/InhalinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inhalink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhalinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
